3129f325154ab670661813237b90b4bf3beb7dba
nanwct <674129850@qq.com>
2022-08-10 20:52:03 +0800

    f1
    f2
    
    f2.1
    
    a=1
    
    a=2

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/nanwct/testGolangThirdModule"
+	"time"
 )
 
 func main() {
 	fmt.Println(testGolangThirdModule.GetTime())
+	go func() {
+		time.Sleep(10)
+	}()
+	fmt.Println("after select!")
+}
+
+func f1()  {
+	a := 2
+	fmt.Println(a)
+}
+
+func f2()  {
+
 }
